8276e06e5e6295fbae7dea6b3b2779ef7172203d
Olivier Laviale <contact@weirdog.com>
2013-07-24 14:28:35 +0200

    Manage block overhaul

--- lib/blocks/manage.php
+++ lib/blocks/manage.php
@@ -11,24 +11,11 @@
 
 namespace Icybee\Modules\Users;
 
+use ICanBoogie\ActiveRecord\Query;
 use ICanBoogie\I18n;
-
-use Brickrouge\Element;
 
 class ManageBlock extends \Icybee\ManageBlock
 {
-	public function __construct($module, array $tags=array())
-	{
-		parent::__construct
-		(
-			$module, $tags + array
-			(
-				self::T_KEY => User::UID,
-				self::T_COLUMNS_ORDER => array(User::USERNAME, User::IS_ACTIVATED, User::EMAIL, 'role', User::CREATED, User::LOGGED_AT)
-			)
-		);
-	}
-
 	static protected function add_assets(\Brickrouge\Document $document)
 	{
 		parent::add_assets($document);
@@ -37,72 +24,87 @@ class ManageBlock extends \Icybee\ManageBlock
 		$document->js->add('manage.js');
 	}
 
-	protected function columns()
+	public function __construct($module, array $attributes=array())
 	{
-		return array
+		parent::__construct
 		(
-			User::USERNAME => array
+			$module, $attributes + array
 			(
-				'label' => 'Username',
-				'ordering' => true
-			),
-
-			User::EMAIL => array
-			(
-				'label' => 'E-Mail'
-			),
-
-			'role' => array
-			(
-				'label' => 'Role',
-				'orderable' => false
-			),
-
-			User::CREATED => array
-			(
-				'class' => 'date'
-			),
-
-			User::LOGGED_AT => array
-			(
-				'class' => 'date'
-			),
-
-			User::IS_ACTIVATED => array
-			(
-				'label' => null,
-				'class' => 'is_activated',
-				'orderable' => false
+				self::T_COLUMNS_ORDER => array(User::USERNAME, User::IS_ACTIVATED, User::EMAIL, 'roles', User::CREATED, User::LOGGED_AT),
+				self::T_ORDER_BY => array('created', 'desc')
 			)
 		);
 	}
 
-	protected function get_query_conditions(array $options)
+	/**
+	 * Adds the following columns:
+	 *
+	 * - `username`: An instance of {@link ManageBlock\UsernameColumn}.
+	 * - `email`: An instance of {@link ManageBlock\EmailColumn}.
+	 * - `roles`: An instance of {@link ManageBlock\RolesColumn}.
+	 * - `created`: An instance of {@link \Icybee\ManageBlock\DateTimeColumn}.
+	 * - `logged_at`: An instance of {@link ManageBlock\LoggedAtColumn}.
+	 * - `is_activated`: An instance of {@link ManageBlock\IsActivatedColumn}.
+	 */
+	protected function get_available_columns()
 	{
-		list($where, $params) = parent::get_query_conditions($options);
-
-		$where[] = 'constructor = ?';
-		$params[] = $this->module->id;
-
-		return array($where, $params);
+		return array_merge(parent::get_available_columns(), array
+		(
+			User::USERNAME => __CLASS__ . '\UsernameColumn',
+			User::EMAIL => __CLASS__ . '\EmailColumn',
+			'roles' => __CLASS__ . '\RolesColumn',
+			User::CREATED => 'Icybee\ManageBlock\DateTimeColumn',
+			User::LOGGED_AT => __CLASS__ . '\LoggedAtColumn',
+			User::IS_ACTIVATED => __CLASS__ . '\IsActivatedColumn'
+		));
 	}
 
-	protected function jobs()
+	/**
+	 * Filters records according to the constructor (the module that created the record).
+	 */
+	protected function alter_query(Query $query, array $filters)
 	{
-		global $core;
+		return parent::alter_query($query, $filters)
+		->filter_by_constructor($this->module->id);
+	}
 
-		// TODO: use parent::jobs()
-
-		$jobs = array
+	/**
+	 * Adds the following jobs:
+	 *
+	 * - `activate`: Activate the selected records.
+	 * - `deactivate`: Deactivate the selected records.
+	 */
+	protected function get_available_jobs()
+	{
+		return array_merge(parent::get_available_jobs(), array
 		(
 			Module::OPERATION_ACTIVATE => I18n\t('activate.operation.title'),
 			Module::OPERATION_DEACTIVATE => I18n\t('deactivate.operation.title')
-		);
-
-		return $jobs;
+		));
 	}
+}
 
-	protected function render_cell_username($record)
+namespace Icybee\Modules\Users\ManageBlock;
+
+use ICanBoogie\ActiveRecord\Model;
+use ICanBoogie\ActiveRecord\Query;
+use ICanBoogie\ActiveRecord\RecordNotFound;
+use ICanBoogie\I18n;
+
+use Brickrouge\Element;
+
+use Icybee\ManageBlock\BooleanColumn;
+use Icybee\ManageBlock\Column;
+use Icybee\ManageBlock\DateTimeColumn;
+use Icybee\ManageBlock\EditDecorator;
+use Icybee\ManageBlock\FilterDecorator;
+
+/**
+ * Representation of the `username` column.
+ */
+class UsernameColumn extends Column
+{
+	public function render_cell($record)
 	{
 		$label = $record->username;
 		$name = $record->name;
@@ -112,10 +114,39 @@ class ManageBlock extends \Icybee\ManageBlock
 			$label .= ' <small>(' . $name . ')</small>';
 		}
 
-		return parent::modify_code($label, $record->uid, $this);
-	}
+		return new Element
+		(
+			'a', array
+			(
+				Element::INNER_HTML => $label,
 
-	protected function render_cell_role($record)
+				'class' => 'edit',
+				'href' => \ICanBoogie\Routing\contextualize("/admin/{$record->constructor}/{$record->uid}/edit"),
+				'title' => I18n\t('manage.edit')
+			)
+		);
+	}
+}
+
+/**
+ * Representation of the `email` column.
+ */
+class EmailColumn extends Column
+{
+	public function render_cell($record)
+	{
+		$email = $record->email;
+
+		return '<a href="mailto:' . $email . '" title="' . $this->manager->t('Send an E-mail') . '">' . $email . '</a>';
+	}
+}
+
+/**
+ * Representation of the `roles` column.
+ */
+class RolesColumn extends Column
+{
+	public function render_cell($record)
 	{
 		if ($record->uid == 1)
 		{
@@ -140,49 +171,201 @@ class ManageBlock extends \Icybee\ManageBlock
 
 		return $label;
 	}
+}
 
-	protected function render_cell_created($record, $property)
+/**
+ * Representation of the `logged_at` column.
+ */
+class LoggedAtColumn extends DateTimeColumn
+{
+	public function render_cell($record)
 	{
-		return $this->render_cell_datetime($record, $property);
-	}
-
-	protected function render_cell_logged_at($record, $property)
-	{
-		$logged_at = $record->$property;
+		$logged_at = $record->logged_at;
 
 		if ($logged_at->is_empty)
 		{
-			return '<em class="small">Never connected</em>';
+			return '<em class="small">' . $this->manager->t("Never connected") . '</em>';
 		}
 
-		return $this->render_cell_datetime($record, $property);
+		return parent::render_cell($record);
+	}
+}
+
+/**
+ * Representation of the `is_activated` column.
+ */
+class IsActivatedColumn extends BooleanColumn
+{
+	public function __construct(\Icybee\ManageBlock $manager, $id, array $options=array())
+	{
+		parent::__construct
+		(
+			$manager, $id, $options + array
+			(
+				'title' => null,
+				'class' => 'cell-fitted',
+				'filters' => array
+				(
+					'options' => array
+					(
+						'=1' => 'Activated',
+						'=0' => 'Deactivated'
+					)
+				)
+			)
+		);
 	}
 
-	protected function render_cell_is_activated($record)
+	public function render_cell($record)
 	{
 		if ($record->is_admin)
 		{
 			return;
 		}
 
-		return new Element
-		(
-			'label', array
-			(
-				Element::CHILDREN => array
-				(
-					new Element
-					(
-						Element::TYPE_CHECKBOX, array
-						(
-							'value' => $record->uid,
-							'checked' => ($record->is_activated != 0)
-						)
-					)
-				),
+		return parent::render_cell($record);
+	}
+}
 
-				'class' => 'checkbox-wrapper circle'
+/**
+ * Representation of a _user_ column.
+ *
+ * This column can be used to represent the users associated to records.
+ */
+class UserColumn extends Column
+{
+	private $user_cache;
+	private $resolved_user_names;
+
+	public function __construct(\Icybee\ManageBlock $manager, $id, array $options=array())
+	{
+		parent::__construct
+		(
+			$manager, $id, $options + array
+			(
+				'title' => 'User',
+				'orderable' => true
 			)
 		);
+
+		$this->resolved_user_names = $this->resolve_user_names($manager->model);
+	}
+
+	private function resolve_user_names(Model $model)
+	{
+		global $core;
+
+		$query = $model->select("DISTINCT `{$this->id}`");
+
+		if ($model->has_scope('own'))
+		{
+			$query = $query->own;
+		}
+
+		if ($model->has_scope('similar_site'))
+		{
+			$query = $query->similar_site;
+		}
+
+		$users_keys = $query->all(\PDO::FETCH_COLUMN);
+
+		if (count($users_keys) < 2)
+		{
+			return;
+		}
+
+		return $core->models['users']
+		->select('uid, IF((firstname != "" AND lastname != ""), CONCAT_WS(" ", firstname, lastname), username) name')
+		->where(array('uid' => $users_keys))
+		->order('name')
+		->pairs;
+	}
+
+	public function alter_query_with_filter(Query $query, $filter_value)
+	{
+		$query = parent::alter_query_with_filter($query, $filter_value);
+
+		if ($filter_value)
+		{
+			$query->where(array($this->id => $filter_value));
+		}
+
+		return $query;
+	}
+
+	public function alter_query_with_order(Query $query, $order_direction)
+	{
+		$keys = array_keys($this->resolved_user_names);
+
+		if ($order_direction < 0)
+		{
+			$keys = array_reverse($keys);
+		}
+
+		return $query->order($this->id, $keys);
+	}
+
+	/**
+	 * Includes the users associated with the records.
+	 */
+	public function alter_records(array $records)
+	{
+		global $core;
+
+		$records = parent::alter_records($records);
+		$keys = array();
+
+		foreach ($records as $record)
+		{
+			$keys[] = $record->{ $this->id };
+		}
+
+		if ($keys)
+		{
+			$keys = array_unique($keys, SORT_NUMERIC);
+
+			try
+			{
+				$this->user_cache = $core->models['users']->find($keys);
+			}
+			catch (RecordNotFound $e)
+			{
+				$this->user_cache = $e->records;
+			}
+		}
+
+		return $records;
+	}
+
+	public function get_options()
+	{
+		if (!$this->resolved_user_names)
+		{
+			return;
+		}
+
+		$options = array();
+
+		foreach ($this->resolved_user_names as $uid => $name)
+		{
+			$options["?uid=" . urlencode($uid)] = $name;
+		}
+
+		return $options;
+	}
+
+	public function render_cell($record)
+	{
+		$uid = $record->{ $this->id };
+		$user = $uid ? $this->user_cache[$uid] : null;
+
+		if (!$user)
+		{
+			return <<<EOT
+<div class="alert alert-error undissmisable">Undefined user: {$uid}</div>
+EOT;
+		}
+
+		return new FilterDecorator($record, $this->id, $this->is_filtering, $user ? $user->name : '');
 	}
 }
--- locale/en.php
+++ locale/en.php
@@ -22,6 +22,15 @@ return array
 		'description.language' => "This is the language to be used for the interface."
 	),
 
+	'users.manage.column' => array
+	(
+		'created' => 'Date created',
+		'email' => 'E-mail',
+		'logged_at' => 'Date logged',
+		'roles' => 'Roles',
+		'username' => 'User name'
+	),
+
 	'label' => array
 	(
 		'name_as' => 'Name as',
@@ -93,14 +102,35 @@ Using the following URL you'll be able to login instantly and update your passwo
 
 This URL can only be used once and is only valid until :until.
 
-If you didn't create a profile neither asked for a new password, this message might be the result
+If you didn't create an account neither asked for a new password, this message might be the result
 of an attack attempt on the website. If you think this is the case, please contact its admin.
 
 The remote address of the request was: :ip.
 EOT
 		),
 
-		'success' => "A message to help you login has been sent to the email address %email."
+		'success' => "A message to help you login has been sent to the email address %email.",
+
+		'unknown_email' => array
+		(
+			'message' => array
+			(
+				'title' => 'Account access attempted',
+				'template' => <<<EOT
+You (or someone else) entered this email address when trying to change the password of an account.
+
+However, this email address is not in our database of registered users and therefore the attempted
+password change has failed.
+
+If you are a user and where expecting this email, please try again using the email address you gave
+when opening your account.
+
+If you are not a user, please ignore this email.
+
+The remote address of the request was: :ip.
+EOT
+			)
+		)
 	),
 
 	'permission.modify own profile' => "The user can modify its profile",
