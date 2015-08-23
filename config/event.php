af09e565802d622253373326c520b513addbeba9
Olivier Laviale <olivier.laviale@gmail.com>
2015-08-23 02:29:43 +0200

    Event config fragments are now their own file

---
+++ config/event.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Icybee\Modules\Users;
+
+use ICanBoogie;
+use Icybee;
+
+$hooks = Hooks::class . '::';
+
+return [
+
+	ICanBoogie\HTTP\AuthenticationRequired::class .'::rescue' => $hooks . 'on_security_exception_rescue',
+	ICanBoogie\HTTP\PermissionRequired::class . '::rescue' => $hooks . 'on_security_exception_rescue',
+	ICanBoogie\Routing\RouteDispatcher::class . '::dispatch:before' => $hooks . 'before_routing_dispatcher_dispatch',
+	Icybee\Modules\Users\Roles\DeleteOperation::class . '::process:before' => $hooks . 'before_roles_delete',
+	Icybee\Modules\Users\WebsiteAdminNotAccessible::class . '::rescue' => $hooks . 'on_website_admin_not_accessible_rescue'
+
+];
--- config/hooks.php
+++ config/hooks.php
@@ -9,16 +9,6 @@ $hooks = Hooks::class . '::';
 
 return [
 
-	'events' => [
-
-		ICanBoogie\HTTP\AuthenticationRequired::class .'::rescue' => $hooks . 'on_security_exception_rescue',
-		ICanBoogie\HTTP\PermissionRequired::class . '::rescue' => $hooks . 'on_security_exception_rescue',
-		ICanBoogie\Routing\RouteDispatcher::class . '::dispatch:before' => $hooks . 'before_routing_dispatcher_dispatch',
-		Icybee\Modules\Users\Roles\DeleteOperation::class . '::process:before' => $hooks . 'before_roles_delete',
-		Icybee\Modules\Users\WebsiteAdminNotAccessible::class . '::rescue' => $hooks . 'on_website_admin_not_accessible_rescue'
-
-	],
-
 	'patron.markups' => [
 
 		'user' => [ $hooks . 'markup_user' ],
