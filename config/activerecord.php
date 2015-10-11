8fecfc6b756ae85f705edff15231693862f95901
Olivier Laviale <olivier.laviale@gmail.com>
2015-10-11 18:22:54 +0200

    Added facets

--- config/activerecord.php
+++ config/activerecord.php
@@ -2,13 +2,19 @@
 
 namespace Icybee\Modules\Users;
 
+use ICanBoogie\Facets\BooleanCriterion;
+use ICanBoogie\Facets\DateTimeCriterion;
+
 return [
 
 	'facets' => [
 
 		'users' => [
 
-			'username' => UsernameCriterion::class
+			'username' => UsernameCriterion::class,
+			'is_activated' => BooleanCriterion::class,
+			'created_at' => DateTimeCriterion::class,
+			'logged_at' => DateTimeCriterion::class
 
 		]
 	]
