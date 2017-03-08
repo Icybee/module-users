44c14ab62eb832fea325ee7f1df9a700d907ad30
Olivier Laviale <olivier.laviale@gmail.com>
2017-03-08 08:58:29 +0100

    Add UserHasNoOwnership

---
+++ lib/UserHasNoOwnership.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the Icybee package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Icybee\Modules\Users;
+
+use ICanBoogie\Accessor\AccessorTrait;
+use ICanBoogie\HTTP\Status;
+
+/**
+ * @property-read User $user
+ * @property-read mixed $resource
+ */
+class UserHasNoOwnership extends \Exception
+{
+	use AccessorTrait;
+
+	const DEFAULT_MESSAGE = 'User has no ownership of the resource.';
+
+	/**
+	 * @var User
+	 */
+	private $user;
+
+	/**
+	 * @return User
+	 */
+	protected function get_user()
+	{
+		return $this->user;
+	}
+
+	/**
+	 * @var mixed
+	 */
+	private $resource;
+
+	/**
+	 * @return mixed
+	 */
+	protected function get_resource()
+	{
+		return $this->resource;
+	}
+
+	/**
+	 * @param User $user
+	 * @param mixed $resource
+	 * @param string $message
+	 * @param \Exception|null $previous
+	 */
+	public function __construct(User $user, $resource, $message = self::DEFAULT_MESSAGE, \Exception $previous = null)
+	{
+		$this->user = $user;
+		$this->resource = $resource;
+
+		parent::__construct($message, Status::FORBIDDEN, $previous);
+	}
+}
---
+++ tests/lib/UserHasNoOwnershipTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Icybee package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Icybee\Modules\Users;
+
+use ICanBoogie\HTTP\Status;
+
+/**
+ * @group ownership
+ */
+class UserHasNoOwnershipTest extends \PHPUnit_Framework_TestCase
+{
+	public function test_exception()
+	{
+		$user = new User;
+		$resource = uniqid();
+		$exception = new UserHasNoOwnership($user, $resource);
+
+		$this->assertSame($user, $exception->user);
+		$this->assertSame($resource, $exception->resource);
+		$this->assertSame(Status::FORBIDDEN, $exception->getCode());
+	}
+}
