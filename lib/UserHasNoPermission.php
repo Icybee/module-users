075041f2584db35dc05b164b9ce549d314d2eda7
Olivier Laviale <olivier.laviale@gmail.com>
2017-03-09 08:48:31 +0100

    Add UserHasNoPermission

---
+++ lib/UserHasNoPermission.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the Icybee package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Icybee\Modules\Users;
+
+use ICanBoogie\Accessor\AccessorTrait;
+use ICanBoogie\HTTP\Status;
+
+/**
+ * @property-read User $user
+ * @property-read string|int $permission
+ * @property-read mixed|null $resource
+ */
+class UserHasNoPermission extends \Exception
+{
+	use AccessorTrait;
+
+	const DEFAULT_MESSAGE = 'User does not have the required permission.';
+
+	/**
+	 * @var User
+	 */
+	private $user;
+
+	/**
+	 * @return User
+	 */
+	protected function get_user()
+	{
+		return $this->user;
+	}
+
+	/**
+	 * @var mixed
+	 */
+	private $permission;
+
+	/**
+	 * @return string|int
+	 */
+	protected function get_permission()
+	{
+		return $this->permission;
+	}
+
+	/**
+	 * @var mixed
+	 */
+	private $resource;
+
+	/**
+	 * @return mixed
+	 */
+	protected function get_resource()
+	{
+		return $this->resource;
+	}
+
+	/**
+	 * @param User $user
+	 * @param string|int $permission
+	 * @param mixed $resource
+	 * @param string $message
+	 * @param \Exception|null $previous
+	 */
+	public function __construct(
+		User $user,
+		$permission,
+		$resource = null,
+		$message = self::DEFAULT_MESSAGE,
+		\Exception $previous = null
+	) {
+		$this->user = $user;
+		$this->permission = $permission;
+		$this->resource = $resource;
+
+		parent::__construct($message, Status::FORBIDDEN, $previous);
+	}
+}
---
+++ tests/lib/UserHasNoPermissionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Icybee package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Icybee\Modules\Users;
+
+use ICanBoogie\HTTP\Status;
+
+/**
+ * @group permission
+ */
+class UserHasNoPermissionTest extends \PHPUnit_Framework_TestCase
+{
+	public function test_exception()
+	{
+		$user = new User;
+		$permission = uniqid();
+		$resource = uniqid();
+		$exception = new UserHasNoPermission($user, $permission, $resource);
+
+		$this->assertSame($user, $exception->user);
+		$this->assertSame($permission, $exception->permission);
+		$this->assertSame($resource, $exception->resource);
+		$this->assertSame(Status::FORBIDDEN, $exception->getCode());
+	}
+}
